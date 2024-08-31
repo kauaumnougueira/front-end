d662b71a08a764f1b900b89ca29929a82fab9293
João Victor <jvvictorls@hotmail.com>
2024-08-31 19:33:36 -0300

    Feat: Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,44 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="p-4 container flex flex-row">
+    <header
+      className="min-w-full container flex flex-row margin shadow-md items-center"
+    >
       <div>
         <img
           src="../../public/logo.png"
           alt=""
-          className="max-w-48"
+          className="max-w-48 px-4"
         />
       </div>
-      <nav className="container flex flex-row bg-red-700">
-        <ul className="container flex flex-row justify-evenly">
+      <nav className="container flex flex-row ">
+        <ul className="container flex flex-row justify-evenly justify-items">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Quem somos</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/estacoes">Estações Tech</Link>
+          </li>
+          <li>
+            <Link to="/voluntario">Seja Voluntário</Link>
+          </li>
+          <li>
+            <Link to="/contato">Contato</Link>
           </li>
         </ul>
       </nav>
+      <div className="flex flex-row py-8 px-8 justify-between w-2/6">
+        <button
+          className="bg-green-600 text-white rounded-3xl px-8 py-2"
+        >
+          Gestor
+        </button>
+        <button
+          className="bg-green-600 text-white rounded-3xl px-8"
+        >
+          Aluno
+        </button>
+      </div>
     </header>
   );
 }
