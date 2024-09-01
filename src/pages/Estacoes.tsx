3174fb5a607cfa0e34a1c52937806671059b3df0
João Victor <jvvictorls@hotmail.com>
2024-09-01 16:10:44 -0300

    Feat: MVP para apresentação

--- src/pages/Estacoes.tsx
+++ src/pages/Estacoes.tsx
@@ -1,104 +1,114 @@
-import React from "react";
-
 // Definindo a interface para uma Estação
 interface Estacao {
-    nome: string;
-    horario: string;
-    contato: string;
-    status: boolean;
-    endereco_id: number;
-    gestor_id: number;
-    created_at: string;
-    updated_at: string;
+  nome: string;
+  horario: string;
+  contato: string;
+  status: boolean;
+  endereco_id: number;
+  gestor_id: number;
+  created_at: string;
+  updated_at: string;
 }
 
-const Estacoes: React.FC = () => {
-    // Lista de estações
-    const estacoes: Estacao[] = [
-        {
-            nome: "Estação Tech Sol e Mar",
-            horario: "08:00 - 18:00",
-            contato: "solmar@contato.com",
-            status: true,
-            endereco_id: 1,
-            gestor_id: 1,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        },
-        {
-            nome: "Estação Tech Nova Vida",
-            horario: "09:00 - 17:00",
-            contato: "novavida@contato.com",
-            status: true,
-            endereco_id: 2,
-            gestor_id: 2,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        },
-        {
-            nome: "Estação Tech Itapera",
-            horario: "10:00 - 16:00",
-            contato: "itapera@contato.com",
-            status: true,
-            endereco_id: 3,
-            gestor_id: 3,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        },
-        {
-            nome: "Estação Tech Maracanã",
-            horario: "07:00 - 19:00",
-            contato: "maracana@contato.com",
-            status: true,
-            endereco_id: 4,
-            gestor_id: 4,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        },
-        {
-            nome: "Estação Tech Vila São José II",
-            horario: "08:00 - 18:00",
-            contato: "vilasaojose@contato.com",
-            status: true,
-            endereco_id: 5,
-            gestor_id: 5,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        },
-        {
-            nome: "Estação Tech Forquilha",
-            horario: "08:00 - 18:00",
-            contato: "forquilha@contato.com",
-            status: false,
-            endereco_id: 6,
-            gestor_id: 3,
-            created_at: "2024-09-01T00:00:00",
-            updated_at: "2024-09-01T00:00:00"
-        }
-    ];
+function Estacoes() {
+  // Lista de estações
+  const estacoes: Estacao[] = [
+    {
+      nome: 'Estação Tech Sol e Mar',
+      horario: '08:00 - 18:00',
+      contato: 'solmar@contato.com',
+      status: true,
+      endereco_id: 1,
+      gestor_id: 1,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+    {
+      nome: 'Estação Tech Nova Vida',
+      horario: '09:00 - 17:00',
+      contato: 'novavida@contato.com',
+      status: true,
+      endereco_id: 2,
+      gestor_id: 2,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+    {
+      nome: 'Estação Tech Itapera',
+      horario: '10:00 - 16:00',
+      contato: 'itapera@contato.com',
+      status: true,
+      endereco_id: 3,
+      gestor_id: 3,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+    {
+      nome: 'Estação Tech Maracanã',
+      horario: '07:00 - 19:00',
+      contato: 'maracana@contato.com',
+      status: true,
+      endereco_id: 4,
+      gestor_id: 4,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+    {
+      nome: 'Estação Tech Vila São José II',
+      horario: '08:00 - 18:00',
+      contato: 'vilasaojose@contato.com',
+      status: true,
+      endereco_id: 5,
+      gestor_id: 5,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+    {
+      nome: 'Estação Tech Forquilha',
+      horario: '08:00 - 18:00',
+      contato: 'forquilha@contato.com',
+      status: false,
+      endereco_id: 6,
+      gestor_id: 3,
+      created_at: '2024-09-01T00:00:00',
+      updated_at: '2024-09-01T00:00:00',
+    },
+  ];
 
-    return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Todas as Estações</h1>
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-6">Todas as Estações</h1>
 
-            {/* Grid de Estações */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {estacoes.map(estacao => (
-                    <div
-                        key={estacao.endereco_id}
-                        className="p-5 bg-cinza mx-2 my-2 rounded shadow-lg"
-                    >
-                        <h2 className="whitespace-nowrap text-lg font-semibold mb-2">{estacao.nome}</h2>
-                        <div className="text-sm">
-                            <p><strong>Horário:</strong> {estacao.horario}</p>
-                            <p className="whitespace-nowrap"><strong>Contato:</strong> {estacao.contato}</p>
-                            <p><strong>Status:</strong> {estacao.status ? 'Ativa' : 'Inativa'}</p>
-                        </div>
-                    </div>
-                ))}
+      {/* Grid de Estações */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {estacoes.map((estacao) => (
+          <div
+            key={ estacao.endereco_id }
+            className="p-4 bg-gray-200 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+          >
+            <h2 className="text-lg font-semibold mb-2">{estacao.nome}</h2>
+            <div className="text-sm">
+              <p>
+                <strong>Horário:</strong>
+                {' '}
+                {estacao.horario}
+              </p>
+              <p className="whitespace-nowrap">
+                <strong>Contato:</strong>
+                {' '}
+                {estacao.contato}
+              </p>
+              <p>
+                <strong>Status:</strong>
+                {' '}
+                {estacao.status ? 'Ativa' : 'Inativa'}
+              </p>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Estacoes;
--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -1,38 +1,42 @@
-const Sobre = () => {
-    return (
-        <div className="items-center w-3/4 bg-mainGray shadow-custom rounded-2xl mt-20 mb-20">
-            <div className="flex">
-                <div
-                    className="flex items-center w-4/5
-           justify-center p-8 mx-8 rounded-full "
-                >
-                    <img
-                        src="/estacao-tech (1).jpeg"
-                        alt="foto da estacao tech"
-                        className="rounded-full"
-                    />
-                </div>
-                <div className="justify-between bg-yellow-700 pt-8">
-                    <figure className="flex flex-row items-center">
-                        <img
-                            src="/figura-001.png"
-                            alt="Figura de um rapaz com lupa"
-                            className="px-4 w-1/6 h-1/6"
-                        />
-                        <h1 className="px-2 py-4 text-3xl">O que é?</h1>
-                    </figure>
-                    <p className="px-4 py-12 text-xl h-1/2">
-                        As Estações Tech são mais que centros tecnológicos;
-                        são espaços de reconhecimento de saberes e desenvolvimento
-                        comunitário, que, além de ultrapassarem o espaço físico,
-                        promovem ideias, soluções, e o desenvolvimento
-                        socioeconômico e ambiental das comunidades locais, apoiando
-                        o cotidiano.
-                    </p>
-                </div>
-            </div>
+function Sobre() {
+  return (
+    <div
+      className="bg-mainGray shadow-custom rounded-2xl mt-8
+     mb-8 mx-4 sm:mx-auto sm:w-3/4 p-4 sm:p-8"
+    >
+      <div className="flex flex-col sm:flex-row items-center">
+        {/* Imagem da Estação Tech */}
+        <div className="flex justify-center w-full sm:w-1/2 mb-4 sm:mb-0">
+          <img
+            src="/estacao-tech (1).jpeg"
+            alt="foto da estacao tech"
+            className="rounded-full w-40 h-40 sm:w-64 sm:h-64"
+          />
         </div>
-    )
+
+        {/* Conteúdo Descritivo */}
+        <div className="w-full sm:w-1/2 bg-yellow-700 p-4 sm:p-8 rounded-lg">
+          <figure className="flex flex-col sm:flex-row items-center mb-6">
+            <img
+              src="/figura-001.png"
+              alt="Figura de um rapaz com lupa"
+              className="w-16 h-16 sm:w-24 sm:h-24 mb-4 sm:mb-0"
+            />
+            <h1 className="text-2xl sm:text-3xl font-bold">
+              O que é?
+            </h1>
+          </figure>
+          <p className="text-base sm:text-xl">
+            As Estações Tech são mais que centros tecnológicos;
+            são espaços de reconhecimento de saberes e desenvolvimento
+            comunitário, que, além de ultrapassarem o espaço físico, promovem
+            ideias, soluções, e o desenvolvimento socioeconômico e ambiental das
+            comunidades locais, apoiando o cotidiano.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sobre
+export default Sobre;
